571c7511fe21ba55e487ceeab7097f8216556f68
Dan Harrin <git@DanHarrin.com>
2020-05-07 08:57:58 +0100

    Alphabetical NPM dependencies

--- src/TallPreset.php
+++ src/TallPreset.php
@@ -9,12 +9,12 @@ use Laravel\Ui\Presets\Preset;
 class TallPreset extends Preset
 {
     const NPM_PACKAGES_TO_ADD = [
-        'laravel-mix-purgecss' => '^4.1',
-        'laravel-mix-tailwind' => '^0.1.0',
-        'tailwindcss' => '^1.0',
         '@tailwindcss/custom-forms' => '^0.2',
         '@tailwindcss/ui' => '^0.1',
         'alpinejs' => '^2.0',
+        'laravel-mix-purgecss' => '^4.1',
+        'laravel-mix-tailwind' => '^0.1.0',
+        'tailwindcss' => '^1.0',
     ];
 
     const NPM_PACKAGES_TO_REMOVE = [
