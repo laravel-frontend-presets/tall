48bd6c68bc231e4a9e406eb0b433d9289499e356
P. K. Tharindu <pktharindu@outlook.com>
2020-05-09 16:53:17 +0530

    add layout to route group

--- stubs/auth/routes/web.php
+++ stubs/auth/routes/web.php
@@ -15,38 +15,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-Route::middleware('guest')->group(function () {
-    Route::livewire('login', 'auth.login')
-        ->layout('layouts.auth')
-        ->name('login');
+Route::layout('layouts.auth')->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::livewire('login', 'auth.login')
+            ->name('login');
 
-    Route::livewire('register', 'auth.register')
-        ->layout('layouts.auth')
-        ->name('register');
+        Route::livewire('register', 'auth.register')
+            ->name('register');
+    });
+
+    Route::livewire('password/reset', 'auth.passwords.email')
+        ->name('password.request');
+
+    Route::livewire('password/reset/{token}', 'auth.passwords.reset')
+        ->name('password.reset');
+
+    Route::middleware('auth')->group(function () {
+        Route::livewire('email/verify', 'auth.verify')
+            ->middleware('throttle:6,1')
+            ->name('verification.notice');
+
+        Route::livewire('password/confirm', 'auth.passwords.confirm')
+            ->name('password.confirm');
+    });
 });
 
-Route::livewire('password/reset', 'auth.passwords.email')
-    ->layout('layouts.auth')
-    ->name('password.request');
-
-Route::livewire('password/reset/{token}', 'auth.passwords.reset')
-    ->layout('layouts.auth')
-    ->name('password.reset');
-
 Route::middleware('auth')->group(function () {
-    Route::livewire('email/verify', 'auth.verify')
-        ->layout('layouts.auth')
-        ->middleware('throttle:6,1')
-        ->name('verification.notice');
-
     Route::get('email/verify/{id}/{hash}', 'Auth\EmailVerificationController')
         ->middleware('signed')
         ->name('verification.verify');
 
     Route::post('logout', 'Auth\LogoutController')
         ->name('logout');
-
-    Route::livewire('password/confirm', 'auth.passwords.confirm')
-        ->layout('layouts.auth')
-        ->name('password.confirm');
 });
