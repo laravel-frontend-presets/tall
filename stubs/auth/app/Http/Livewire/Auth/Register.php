873c7a77e27549ee0d01991a9c416e2e0b2ab2f1
Ryan Chandler <ryan@chandler.codes>
2020-09-09 11:18:55 +0100

    fix(model namespaces for L8)

--- stubs/auth/app/Http/Livewire/Auth/Register.php
+++ stubs/auth/app/Http/Livewire/Auth/Register.php
@@ -3,7 +3,7 @@
 namespace App\Http\Livewire\Auth;
 
 use App\Providers\RouteServiceProvider;
-use App\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
