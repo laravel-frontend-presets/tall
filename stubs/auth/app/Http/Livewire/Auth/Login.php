fa1af0dc84355a65f516f089a405fb64fd451dd4
Ryan Chandler <ryan@chandler.codes>
2020-09-09 12:05:25 +0100

    fix(add missing returns in redirect statements)

--- stubs/auth/app/Http/Livewire/Auth/Login.php
+++ stubs/auth/app/Http/Livewire/Auth/Login.php
@@ -30,7 +30,7 @@ class Login extends Component
             return;
         }
 
-        redirect()->intended(route('home'));
+        return redirect()->intended(route('home'));
     }
 
     public function render()
