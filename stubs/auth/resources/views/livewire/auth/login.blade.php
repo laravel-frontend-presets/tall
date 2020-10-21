fd338e9b9762aefaa8ddef41c21d5ead0d07ed2c
Dima <dima.botezatu@gmail.com>
2020-10-21 16:09:42 +0300

    When route "register" not exists
    When route "register" not exists, on login page whe need to check if route register exists

--- stubs/auth/resources/views/livewire/auth/login.blade.php
+++ stubs/auth/resources/views/livewire/auth/login.blade.php
@@ -9,12 +9,14 @@
         <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900 leading-9">
             Sign in to your account
         </h2>
-        <p class="mt-2 text-sm text-center text-gray-600 leading-5 max-w">
-            Or
-            <a href="{{ route('register') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                create a new account
-            </a>
-        </p>
+        @if (Route::has('register'))
+            <p class="mt-2 text-sm text-center text-gray-600 leading-5 max-w">
+                Or
+                <a href="{{ route('register') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                    create a new account
+                </a>
+            </p>
+        @endif
     </div>
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
