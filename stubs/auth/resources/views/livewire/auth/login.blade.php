c7a98383ae278ee1373693e901507a76351597aa
Marco Rieser <marco.rieser@jedesign.ch>
2020-11-20 22:16:43 +0100

    Fix indentations in login.blade.php

--- stubs/auth/resources/views/livewire/auth/login.blade.php
+++ stubs/auth/resources/views/livewire/auth/login.blade.php
@@ -30,7 +30,7 @@
                     </div>
 
                     @error('email')
-                    <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                     @enderror
                 </div>
 
@@ -44,7 +44,7 @@
                     </div>
 
                     @error('password')
-                    <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                     @enderror
                 </div>
 
