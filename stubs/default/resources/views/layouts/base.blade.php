1404cc4523a3c9931b53387e24fad93aea963296
Qisthi Ramadhani <realramageek@gmail.com>
2021-02-20 15:22:04 +0700

    implement to blade

--- stubs/default/resources/views/layouts/base.blade.php
+++ stubs/default/resources/views/layouts/base.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html
+    :class="{ 'theme-dark': dark }"
+    x-data="window.initAlpine()"
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @hasSection('title')
 
+        @hasSection('title')
             <title>@yield('title') - {{ config('app.name') }}</title>
         @else
             <title>{{ config('app.name') }}</title>
