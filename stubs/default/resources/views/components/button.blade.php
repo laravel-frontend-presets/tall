f92a324cc593eaa045de353d8e816a813154b0f2
Jeff Greco <jeff@jeffpgreco.com>
2021-11-24 23:54:59 +0000

    Add button component.

---
+++ stubs/default/resources/views/components/button.blade.php
@@ -0,0 +1,3 @@
+<button {{$attributes->merge(['type'=>'submit','class'=>'flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring-indigo active:bg-indigo-700 transition duration-150 ease-in-out'])}}>
+    {{$slot}}
+</button>
