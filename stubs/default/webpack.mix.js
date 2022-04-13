0d43d06329bf0c3c91bd9aaed2f4f6639d6d65bb
safeboot <mynamesafeboot@gmail.com>
2022-04-13 21:32:19 +0200

    Added CSS into webpack's mix.

--- stubs/default/webpack.mix.js
+++ stubs/default/webpack.mix.js
@@ -15,6 +15,7 @@ require("laravel-mix-tailwind");
 
 mix.js("resources/js/app.js", "public/js/app.js")
     .sass("resources/sass/app.scss", "public/css/app.css")
+    .css("resources/css/app.css", "public/css/app.css")
     .tailwind("./tailwind.config.js")
     .sourceMaps();
 
