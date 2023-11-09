30e5a87a2c3f63e3f146204178f85b16f5ed0c59
Bhargav Raviya <46953122+bhargavraviya@users.noreply.github.com>
2023-11-09 17:47:32 +0530

    remove presets version

--- stubs/default/resources/views/welcome.blade.php
+++ stubs/default/resources/views/welcome.blade.php
@@ -109,9 +109,8 @@
                     </div>
                 </div>
                 <div class="ml-4 text-sm text-center text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) & TALL
-                    {{ \Composer\InstalledVersions::getPrettyVersion('laravel-frontend-presets/tall') }} (Livewire
-                    {{ \Composer\InstalledVersions::getPrettyVersion('livewire/livewire') }})
+                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) & Livewire
+                    {{ \Composer\InstalledVersions::getPrettyVersion('livewire/livewire') }}
                 </div>
             </div>
         </div>
