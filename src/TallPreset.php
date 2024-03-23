fc2de9167407090b42540a467a95ac04ada410ce
Maruf Alom <desk.maruf@gmail.com>
2024-03-24 02:09:44 +0600

    feat: added if RouteServiceProvider exists

--- src/TallPreset.php
+++ src/TallPreset.php
@@ -2,9 +2,9 @@
 
 namespace LaravelFrontendPresets\Tall;
 
-use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Arr;
 use Laravel\Ui\Presets\Preset;
+use Illuminate\Filesystem\Filesystem;
 
 class TallPreset extends Preset
 {
@@ -23,21 +23,33 @@ class TallPreset extends Preset
         'axios',
     ];
 
-    public static function install()
+    public static function install() : void
     {
         static::updatePackages();
 
-        $filesystem = new Filesystem();
+        $filesystem = new Filesystem;
         $filesystem->deleteDirectory(resource_path('sass'));
         $filesystem->copyDirectory(__DIR__ . '/../stubs/default', base_path());
 
-        static::updateFile(base_path('app/Providers/RouteServiceProvider.php'), function ($file) {
-            return str_replace("public const HOME = '/home';", "public const HOME = '/';", $file);
-        });
+        if (file_exists(base_path('app/Providers/RouteServiceProvider.php'))) {
+            static::updateFile(base_path('app/Providers/RouteServiceProvider.php'), static function ($file) {
+                return str_replace(
+                    "public const HOME = '/home';",
+                    "public const HOME = '/';",
+                    $file
+                );
+            });
+        }
 
-        static::updateFile(base_path('app/Http/Middleware/RedirectIfAuthenticated.php'), function ($file) {
-            return str_replace("RouteServiceProvider::HOME", "route('home')", $file);
-        });
+        if (file_exists(base_path('app/Http/Middleware/RedirectIfAuthenticated.php'))) {
+            static::updateFile(base_path('app/Http/Middleware/RedirectIfAuthenticated.php'), static function ($file) {
+                return str_replace(
+                    "RouteServiceProvider::HOME",
+                    "route('home')",
+                    $file
+                );
+            });
+        }
     }
 
     public static function installAuth()
